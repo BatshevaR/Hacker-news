10949dd627acc05d22f385794294242694a20bac
Batsheva Ruvel <bat-sheva.ruvel@mksinst.com>
2021-12-20 15:03:53 +0200

    Initial Commit

---
+++ Hacker news/Hacker_newsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hacker_newsApp.swift
+//  Hacker news
+//
+//  Created by ophir hartom on 20/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct Hacker_newsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
